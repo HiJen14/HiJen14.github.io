cb9d6df42ac3efcb87e7e700dd062f9cbb09ccd0
Jennifer <52807299+The0ther0ne@users.noreply.github.com>
2021-03-11 22:07:26 +0000

    script

---
+++ assets/scripts/dynamic.js
@@ -0,0 +1,22 @@
+var black : boolean = true;
+   
+		function ChangeImage()
+		{
+			if(black)
+			{
+				document.getElementById("LinkHD").src = {{ site.baseurl }}"/Images/LinkGrey.png"; 
+				black = false;
+			}
+			else
+			{
+				document.getElementById("LinkHD").src = {{ site.baseurl }}"/Images/LinkBlack.png"; 
+				black = true;
+			}
+		}
+   
+		//Hey Listen
+		function OpenNavi() 
+		{
+			document.getElementById("mySidebar").style.width = "250px";
+			document.getElementById("main").style.marginLeft = "250px";
+		}
